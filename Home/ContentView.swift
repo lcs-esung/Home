880ef744c7e00e10c3deba343898013eb4f2390e
Edna Sung <esung@lcs.on.ca>
2024-01-12 09:38:27 -0500

    Finished on the gradient.

--- Home/ContentView.swift
+++ Home/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: Stored properties
-    let firstGradientColors = Gradient(colors: [.white, .blue])
+    let firstGradientColors = Gradient(colors: [.lightBlue, .deepBlue, .deepPurple, .clear])
+    
+    let secondGradientColors = Gradient(colors: [.greyYellow, .deepPurple, .clear])
     
     // MARK: Computed properties
 
@@ -18,16 +20,26 @@ struct ContentView: View {
         ZStack {
             
             // Background
-            Color.orange
+            Color.black
                 .ignoresSafeArea()
             
             //First gradient
             RadialGradient(
-                gradient:firstGradientColors,
-                center: .center,
-                startRadius: 100,
-                endRadius: 300
+                gradient: firstGradientColors,
+                center: UnitPoint(x: 0.1, y: 0),
+                startRadius: 0,
+                endRadius: 1000
             )
+            .ignoresSafeArea()
+            
+            //Second gradient
+            RadialGradient(
+                gradient: secondGradientColors,
+                center: UnitPoint(x: 1, y: 1),
+                startRadius: 0,
+                endRadius: 900
+            )
+            .ignoresSafeArea()
             
             // Foreground
             VStack {

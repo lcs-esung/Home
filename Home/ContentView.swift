603b523746836ef408538011479025bd80b1b44f
Edna Sung <esung@lcs.on.ca>
2024-01-12 08:53:29 -0500

    First gradient colors added.

--- Home/ContentView.swift
+++ Home/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    let firstGradientColors = Gradient(colors: [Color.blue, Color.yellow])
+    
+    // MARK: Computed properties
+
     var body: some View {
         ZStack {
             
             // Background
             LinearGradient(
-                gradient: /*@START_MENU_TOKEN@*/Gradient(colors: [Color.red, Color.blue])/*@END_MENU_TOKEN@*/,
-                startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/,
-                endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/
+                gradient: firstGradientColors,
+                startPoint: .leading,
+                endPoint: .bottom
             )
             .ignoresSafeArea()
         
